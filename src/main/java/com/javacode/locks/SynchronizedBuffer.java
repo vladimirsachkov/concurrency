12549ab4fce42a45422c7da0b8dcab6b1cbad0c0
alexa <alexanderredar1@gmail.com>
2023-07-20 19:38:50 +0300

    [8] - ReentrantLock класс.

---
+++ src/main/java/com/javacode/locks/SynchronizedBuffer.java
@@ -0,0 +1,67 @@
+package com.javacode.locks;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.javacode.threadslesson.ColorScheme.BLUE;
+import static com.javacode.threadslesson.ColorScheme.RED;
+
+public class SynchronizedBuffer {
+
+    private static final Lock monitor = new ReentrantLock();
+
+    private static final Condition canRead = monitor.newCondition();
+    private static final Condition canWrite = monitor.newCondition();
+
+    private static int buffer = 0;
+    private static boolean isEmpty = true;
+
+    public static void main(String[] args) {
+        new Thread(SynchronizedBuffer::blockingWrite).start();
+        new Thread(SynchronizedBuffer::blockingRead).start();
+    }
+
+    private static void blockingWrite() {
+        for (int i = 0; i < 10; i++) {
+            monitor.lock();
+            try {
+                while (!isEmpty) {
+                    System.out.println(RED+"Writer is trying to access a resource");
+                    System.out.println(RED+"Buffer is full");
+                    canWrite.await(5, TimeUnit.SECONDS);
+                }
+                buffer++;
+                isEmpty = false;
+                System.out.println(RED + "Writer produced: " + buffer);
+                canRead.signal();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                monitor.unlock();
+            }
+        }
+    }
+
+    private static void blockingRead() {
+        for (int i = 0; i < 10; i++) {
+            monitor.lock();
+            try {
+                while (isEmpty) {
+                    System.out.println(BLUE+"Reader is trying to access a resource");
+                    System.out.println(BLUE+"Buffer is empty");
+                    canWrite.await(5, TimeUnit.SECONDS);
+                }
+                int readValue = buffer;
+                isEmpty = true;
+                System.out.println(BLUE+"Reader reads from buffer: " + readValue);
+                canWrite.signal();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                monitor.unlock();
+            }
+        }
+    }
+}
